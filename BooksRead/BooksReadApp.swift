3f9f14ce2e8a3b21eaf59203142616c722d5cd0f
Linas <linas404@protonmail.com>
2025-04-29 19:04:27 +0200

    Initial Commit

---
+++ BooksRead/BooksReadApp.swift
@@ -0,0 +1,17 @@
+//
+//  BooksReadApp.swift
+//  BooksRead
+//
+//  Created by Linas on 29/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct BooksReadApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
